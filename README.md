f713e347757d6bb8a16029fa5778be2d9a6994b8
ivan <ivan@IvanPC.lan>
2020-07-08 01:23:47 +0200

    Update v1.1

--- README.md
+++ README.md
@@ -23,7 +23,7 @@ El script de instalación se ocupará del resto.
 ## Instalación
 Descarga el repositorio posteado en esta misma página, descomprímela y entra a ella en una terminal.
 
-Una vez compruebes que cumples los requisitos mencionados en el apartado de requisitos técnicos, ejecuta `python3 installer.py` y el instalador te guiará a través de toda la información que debes darle. Cuando éste termine tendrás un cuentamuertes funcional en la dirección web en el dominio que has especificado, en http. Si quieres aplicarle https, te sugiero que eches un ojo al proyecto de [Certbot](https://certbot.eff.org/), de la Electronic Frontier Foundation.
+Una vez compruebes que cumples los requisitos mencionados en el apartado de requisitos técnicos, ejecuta `python3 installer.py` y el instalador te guiará a través de toda la información que debes darle. Cuando éste termine tendrás un cuentamuertes funcional en la dirección web en el dominio que has especificado, en http. Si quieres aplicarle https, te sugiero que eches un ojo al proyecto de [link](https://certbot.eff.org/ "Certbot"), de la Electronic Frontier Foundation.
 
 ## Manual
 
@@ -46,6 +46,15 @@ En el salón de las estadísticas podemos ver información de estadísticas sobr
 Este panel muestra un historial de todas las muertes registradas en el cuentamuertes ordenado desde la más reciente primero. Nos muestra su id, su fecha y hora, su juego y su sesión, y nos da un botón que nos permite eliminar esta muerte del registro en caso de que se contabilizara de forma errónea.
 
 Este apartado tiene un sistema que muestra **todas** las muertes registradas en la base de datos, porlo que hay que tener en cuenta que si el volumen de muertes es muy elevado, quizá esta página podría tardar un poco en generarse, es algo a vigilar y tener en cuenta.
+
+### Integración con el chatbot de StreamElements
+Si utilizas en tu canal el chatbot de [StreamElements](https://streamelements.com/), tienes disponible una API de integración con tu chat para que puedan consultar muertes sin necesidad de darles la página del cuentamuertes y exponerla.
+
+Esto se basa en 2 comandos específicos: Uno que nos dará la lista de juegos a consultar, y uno que nos mostrará estadísticas del juego deseado.
+
+En el caso del primero, añadiremos `${customapi.https://{Tu dominio elegido}/chatbot/juegos.php}` como comando, del estilo `!listajuegos`, y éste nos devolverá el listado de juegos registrados, por ejemplo `Hollow Knight,NieR: Automata ruta B, NieR: Automata ruta C`, separando cada entrada posibbble por comas.
+
+Con los datos de este podremos utilizar el segundo comando que añadiremos, `${customapi.https://{Tu dominio elegido}/chatbot/total.php?game=$(queryencode $(1:))}`, con un comando del estilo `!muertesjuego`, el cual requiere como entrada uno de los juegos devueltos por la lista del comando anterior. Debe estar escrito exactamente igual que nos indica el comando. Si por ejemplo introducimos `!muertesjuego Hollow Knight` nos devolverá un texto del estilo de `tu streamer de confianza lleva 433 muertes en Hollow Knight (3 muertes en la última sesión registrada), que representa un 90.02% de las muertes registradas.`. En el caso de que el juego que pidamos esté mal escrito, o sea un juego que no exisste en la base de datos, se nos devolverá `ERROR, ese jeugo no está en la lista de juegos posibles.`.
 
 ## Contribuciones
 Por el momento las contribuciones a este web project personal están cerradas, pero siempre puedes abrir una Issue en GitHub si encuentras algún error que intentaré solucionar en la medida de lo posible.
--- install/installer.py
+++ install/installer.py
@@ -18,107 +18,131 @@ seguro = input("Quieres continuar? [s/N]")
 if seguro not in ["s", "S"]:
 	exit()
 
-#Si se quiere continuar, empieza el script
-print("Se va a continuar con la instalación!")
-time.sleep(2)
-print("Primero comprobaremos que instalas todo lo necesario...")
-time.sleep(2)
+#Preguntar si se quiere Instalar el cuentamuertes de cero o actualizar una versión ya instalada
+upgrade = input("Quieres instalar una instancia nueva [i], o actualizar una existente [a]?")
+while upgrade not in ["i", "a", "I", "A"]:
+	upgrade = input("Opción no válda. Quieres instalar una instancia nueva [i], o actualizar una existente [a]?")
 
-#Actualizar e instalar los paquetes necesarios
-os.system("apt update")
-os.system("apt install -y python3-psycopg2 php-pgsql php")
-print("\nInstalado todo lo necesario! Ahora se recargará Apache2.")
-time.sleep(2)
-import psycopg2
+if upgrade in ["i", "I"]: #Opción de instalación completa
+	#Si se quiere continuar, empieza el script de instalación
+	print("Se va a continuar con la instalación!")
+	time.sleep(2)
+	print("Primero comprobaremos que instalas todo lo necesario...")
+	time.sleep(2)
 
-#Reiniciar Apache2
-os.system("service apache2 reload")
-print("\nExtrayendo carpeta de contenidos.")
-time.sleep(2)
+	#Actualizar e instalar los paquetes necesarios
+	os.system("apt update")
+	os.system("apt install -y python3-psycopg2 php-pgsql php")
+	print("\nInstalado todo lo necesario! Ahora se recargará Apache2.")
+	time.sleep(2)
+	import psycopg2
 
-#Extraer a su carpeta los archivos de la webApp
-os.system("mkdir source/apache")
-os.system("tar -xvzf source/files.tar.gz -C source/apache")
+	#Reiniciar Apache2
+	os.system("service apache2 reload")
+	print("\nExtrayendo carpeta de contenidos.")
+	time.sleep(2)
 
-#Credeciales de la webApp en PostgreSQL
-dbUser = input("\nEscribe el usuario que luego será dueño de la base de datos: ")
-dbPassword = input("Contraseña que el usuario creado usará para que la web se comunique con PostgreSQL (CUIDADO: LA CONTRASEÑA ES VISIBLE EN ESTE PASO): ")
-dbDatabase = input("Nombre de la base de datos que crearás, donde se guardarán los registros para la web: ")
-vhostDomain = input("Dominio que estará en el vhost y que apuntará a esta web: ")
-pageName = input("Nombre de quién lleva el contador, ejemplo escribir 'María' para establecer en la web 'Cuentamuertes de María': ")
-print("Todos los datos recogidos! Hagamos un par de cambios en los archivos para adaptarlos: ")
-time.sleep(2)
+	#Extraer a su carpeta los archivos de la webApp
+	os.system("mkdir source/apache")
+	os.system("tar -xvzf source/files.tar.gz -C source/apache")
 
-#Replace con los datos recibidos para el backend
-replaceUser = "sed -i 's/user=REPLACE/user=" + dbUser + "/g' source/apache/backend/creds/.my.cnf"
-os.system(replaceUser)
-replacePass = "sed -i 's/password=REPLACE/password=" + dbPassword + "/g' source/apache/backend/creds/.my.cnf"
-os.system(replacePass)
-replaceDB = "sed -i 's/database=REPLACE/database=" + dbDatabase + "/g' source/apache/backend/creds/.my.cnf"
-os.system(replaceDB)
-replacePageName = "sed -i 's/name=REPLACE/name=" + pageName + "/g' source/apache/backend/creds/.my.cnf"
-os.system(replacePageName)
+	#Credeciales de la webApp en PostgreSQL
+	dbUser = input("\nEscribe el usuario que luego será dueño de la base de datos: ")
+	dbPassword = input("Contraseña que el usuario creado usará para que la web se comunique con PostgreSQL (CUIDADO: LA CONTRASEÑA ES VISIBLE EN ESTE PASO): ")
+	dbDatabase = input("Nombre de la base de datos que crearás, donde se guardarán los registros para la web: ")
+	vhostDomain = input("Dominio que estará en el vhost y que apuntará a esta web: ")
+	pageName = input("Nombre de quién lleva el contador, ejemplo escribir 'María' para establecer en la web 'Cuentamuertes de María': ")
+	print("Todos los datos recogidos! Hagamos un par de cambios en los archivos para adaptarlos: ")
+	time.sleep(2)
+	
+	#Replace con los datos recibidos para el backend
+	replaceUser = "sed -i 's/user=REPLACE/user=" + dbUser + "/g' source/apache/backend/creds/.my.cnf"
+	os.system(replaceUser)
+	replacePass = "sed -i 's/password=REPLACE/password=" + dbPassword + "/g' source/apache/backend/creds/.my.cnf"
+	os.system(replacePass)
+	replaceDB = "sed -i 's/database=REPLACE/database=" + dbUser + "/g' source/apache/backend/creds/.my.cnf"
+	os.system(replaceDB)
+	replacePageName = "sed -i 's/name=REPLACE/name=" + pageName + "/g' source/apache/backend/creds/.my.cnf"
+	os.system(replacePageName)
+	
+	#Replace para el vHost de Apache2
+	replacevHost = "sed -i 's/    ServerName REPLACE/    ServerName " + vhostDomain + "/g' source/cuentamuertes.conf"
+	os.system(replacevHost)
+	
+	#Replace en el script sql
+	replaceSQL = "sed -i 's/ALTER TABLE public.juegos OWNER TO REPLACEONINSTALL;/ALTER TABLE public.juegos OWNER TO " + dbUser + ";/g' source/db.sql"
+	os.system(replaceSQL)
+	replaceSQL = "sed -i 's/ALTER TABLE public.main OWNER TO REPLACEONINSTALL;/ALTER TABLE public.main OWNER TO " + dbUser + ";/g' source/db.sql"
+	os.system(replaceSQL)
+	replaceSQL = "sed -i 's/ALTER TABLE public.main_id_seq OWNER TO REPLACEONINSTALL;/ALTER TABLE public.main_id_seq OWNER TO " + dbUser + ";/g' source/db.sql"
+	os.system(replaceSQL)
+	#Credenciales de administrador de postgreSQL
+	print("\nAhora necesitamos los credenciales de acceso a la base de datos.")
+	dbAdminUser = input("Nombre del usuario que puede crear usuarios con permisos en el gestor de bases de datos: ")
+	dbAdminPass = input("Contraseña del usuario que puede crear usuarios con permisos en el gestor de bases de datos (CUIDADO: LA CONTRASEÑA ES VISIBLE EN ESTE PASO): ")
+	connString = "host='localhost' dbname='postgres' user='" + dbAdminUser + "' password='" + dbAdminPass + "'"
+	conn = psycopg2.connect(connString)
+	print("Conectado! Ahora el script ejecutará las tareas necesarias: ")
+	cursor = conn.cursor()
+	
+	#Crea el usuario especificado par que sea dueño de la base de datos
+	print("Creando el usuario aislado...")
+	query = "CREATE USER " + dbUser + " WITH CREATEDB ENCRYPTED PASSWORD '" + dbPassword + "'"
+	cursor.execute(query)
+	conn.commit()
+	cursor.close()
+	conn.close()
+	
+	#Crea la database donde se guardarán las muertes, por aislamiento 
+	print("Creando la base de datos...")
+	connString = "host='localhost' dbname='postgres' user='" + dbUser + "' password='" + dbPassword + "'"
+	conn = psycopg2.connect(connString)
+	cursor = conn.cursor()
+	conn.autocommit = True
+	query = "CREATE DATABASE " + dbDatabase
+	cursor.execute(query)
+	cursor.close()
+	conn.autocommit = False
+	conn.close()
 
-#Replace para el vHost de Apache2
-replacevHost = "sed -i 's/    ServerName REPLACE/    ServerName " + vhostDomain + "/g' source/cuentamuertes.conf"
-os.system(replacevHost)
+	#Lee el .sql de las tablas y lo ejecuta para crear las tablas con el owner correcto
+	print("Creando las tablas de la base de datos...")
+	connString = "host='localhost' dbname='" + dbDatabase + "' user='" + dbUser + "' password='" + dbPassword + "'"
+	conn = psycopg2.connect(connString)
+	cursor = conn.cursor()
+	cursor.execute(open("source/db.sql", "r").read())
+	conn.commit()
+	cursor.close()
+	conn.close()
+	print("Se han creado los elementos de bases de datos! Ahora lo moveremos todo a su sitio y podrás crear tu VirtualiHost de Apache2 con webroot en /var/www/cuentamuertes/frontend/, iniciiarlo, y ya estará funcional.")
+	time.sleep(2)
+	
+	#Mover todo a su lugar y cambiar los permisos a los correctos
+	os.system("mkdir /var/www/cuentamuertes")
+	os.system("mv source/apache/* /var/www/cuentamuertes/")
+	os.system("chown root:www-data -R /var/www/cuentamuertes/frontend/")
+	os.system("mv source/cuentamuertes.conf /etc/apache2/sites-available/")
+	os.system("a2ensite cuentamuertes")
+	print("Todo listo! Recargando apache2 para que el site esté activo...")
+	time.sleep(2)
+	os.system("service apache2 reload")
+	print("Listo! Que disfrutes del cuentamuertes! ya puedes borrar esta carpeta de instalación si quieres")
+else: #Opción de actualización del frontend
+	print("Has elegido actualizar la instancia ya instalada! Un momento...")
 
-#Replace en el script sql
-replaceSQL = "sed -i 's/ALTER TABLE public.juegos OWNER TO REPLACEONINSTALL;/ALTER TABLE public.juegos OWNER TO " + dbUser + ";/g' source/db.sql"
-os.system(replaceSQL)
-replaceSQL = "sed -i 's/ALTER TABLE public.main OWNER TO REPLACEONINSTALL;/ALTER TABLE public.main OWNER TO " + dbUser + ";/g' source/db.sql"
-os.system(replaceSQL)
-replaceSQL = "sed -i 's/ALTER TABLE public.main_id_seq OWNER TO REPLACEONINSTALL;/ALTER TABLE public.main_id_seq OWNER TO " + dbUser + ";/g' source/db.sql"
-os.system(replaceSQL)
+	#Extraer a su carpeta los archivos de la webApp
+	os.system("mkdir source/apache")
+	os.system("tar -xvzf source/files.tar.gz -C source/apache")
 
-#Credenciales de administrador de postgreSQL
-print("\nAhora necesitamos los credenciales de acceso a la base de datos.")
-dbAdminUser = input("Nombre del usuario que puede crear usuarios con permisos en el gestor de bases de datos: ")
-dbAdminPass = input("Contraseña del usuario que puede crear usuarios con permisos en el gestor de bases de datos (CUIDADO: LA CONTRASEÑA ES VISIBLE EN ESTE PASO): ")
-connString = "host='localhost' dbname='postgres' user='" + dbAdminUser + "' password='" + dbAdminPass + "'"
-conn = psycopg2.connect(connString)
-print("Conectado! Ahora el script ejecutará las tareas necesarias: ")
-cursor = conn.cursor()
+	#Borrar el frontend anterior
+	os.system("rm -r /var/www/cuentamuertes/frontend/*")
 
-#Crea el usuario especificado par que sea dueño de la base de datos
-print("Creando el usuario aislado...")
-query = "CREATE USER " + dbUser + " WITH CREATEDB ENCRYPTED PASSWORD '" + dbPassword + "'"
-cursor.execute(query)
-conn.commit()
-cursor.close()
-conn.close()
+	#Mover a su sitio el nuevo frontend
+	os.system("mv source/apache/frontend/* /var/www/cuentamuertes/frontend")
+	os.system("chown root:www-data -R /var/www/cuentamuertes/frontend/")
+	print("Todo listo! Recargando apache2 para que el site esté activo...")
+	time.sleep(2)
 
-#Crea la database donde se guardarán las muertes, por aislamiento 
-print("Creando la base de datos...")
-connString = "host='localhost' dbname='postgres' user='" + dbUser + "' password='" + dbPassword + "'"
-conn = psycopg2.connect(connString)
-cursor = conn.cursor()
-conn.autocommit = True
-query = "CREATE DATABASE " + dbDatabase
-cursor.execute(query)
-cursor.close()
-conn.autocommit = False
-conn.close()
-
-#Lee el .sql de las tablas y lo ejecuta para crear las tablas con el owner correcto
-print("Creando las tablas de la base de datos...")
-connString = "host='localhost' dbname='" + dbDatabase + "' user='" + dbUser + "' password='" + dbPassword + "'"
-conn = psycopg2.connect(connString)
-cursor = conn.cursor()
-cursor.execute(open("source/db.sql", "r").read())
-conn.commit()
-cursor.close()
-conn.close()
-print("Se han creado los elementos de bases de datos! Ahora lo moveremos todo a su sitio y podrás crear tu VirtualiHost de Apache2 con webroot en /var/www/cuentamuertes/frontend/, iniciiarlo, y ya estará funcional.")
-time.sleep(2)
-
-#Mover todo a su lugar y cambiar los permisos a los correctos
-os.system("mkdir /var/www/cuentamuertes")
-os.system("mv source/apache/* /var/www/cuentamuertes/")
-os.system("chown root:www-data -R /var/www/cuentamuertes/frontend/")
-os.system("mv source/cuentamuertes.conf /etc/apache2/sites-available/")
-os.system("a2ensite cuentamuertes")
-print("Todo listo! Recargando apache2 para que el site esté activo...")
-time.sleep(2)
-os.system("service apache2 reload")
-print("Listo! Que disfrutes del cuentamuertes! ya puedes borrar esta carpeta de instalación si quieres")
+	#Recargar Apache2 con los nuevos permisos y archivos
+	os.system("service apache2 reload")
+	print("Listo! Que disfrutes del cuentamuertes actualizado! ya puedes borrar esta carpeta de instalación si quieres")
